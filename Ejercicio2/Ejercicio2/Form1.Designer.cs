aed9812cf5cc8aa675d3a9b36be0b113a307b2ef
Mario <mjcarrion@uamv.edu.ni>
2024-09-20 08:06:55 -0600

    Ejercicio numero 2

---
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregarNums = new System.Windows.Forms.Button();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(525, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese dos numeros";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtN
+            // 
+            this.txtN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN.Location = new System.Drawing.Point(531, 158);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(257, 35);
+            this.txtN.TabIndex = 1;
+            // 
+            // txtY
+            // 
+            this.txtY.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtY.Location = new System.Drawing.Point(531, 209);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(257, 35);
+            this.txtY.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(495, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(495, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y";
+            // 
+            // btnAgregarNums
+            // 
+            this.btnAgregarNums.Location = new System.Drawing.Point(531, 260);
+            this.btnAgregarNums.Name = "btnAgregarNums";
+            this.btnAgregarNums.Size = new System.Drawing.Size(257, 44);
+            this.btnAgregarNums.TabIndex = 5;
+            this.btnAgregarNums.Text = "Agregar numeros";
+            this.btnAgregarNums.UseVisualStyleBackColor = true;
+            this.btnAgregarNums.Click += new System.EventHandler(this.btnAgregarNums_Click);
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.ItemHeight = 20;
+            this.lstNumeros.Location = new System.Drawing.Point(531, 357);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(257, 284);
+            this.lstNumeros.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1353, 703);
+            this.Controls.Add(this.lstNumeros);
+            this.Controls.Add(this.btnAgregarNums);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregarNums;
+        private System.Windows.Forms.ListBox lstNumeros;
+    }
+}
+
---
+++ Ejercicio2/Ejercicio2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregarNums_Click(object sender, EventArgs e)
+        {
+            // Limpiar la lista de numeros
+            lstNumeros.Items.Clear();
+
+            // Leer los valores de los TextBox
+            if (int.TryParse(txtN.Text, out int x) && int.TryParse(txtY.Text, out int y))
+            {
+                if (x < y)
+                {
+                    // Mostrar los numeros de x a y (ascendente)
+                    for (int i = x; i <= y; i++)
+                    {
+                        lstNumeros.Items.Add(i); // Agregar a la lista de numeros
+                    }
+                }
+                else
+                {
+                    // Mostrar los numeros de x a y (descendente)
+                    for (int i = x; i >= y; i--)
+                    {
+                        lstNumeros.Items.Add(i); // Agregar a la lista de numeros
+                    }
+                }
+            }
+            else
+            {
+                // Mensaje de error si los valores no son validos
+                MessageBox.Show("Por favor ingrese números validos en ambos campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
