b804db5f346c2ebb62f17332945ee65e028c7c54
cfbj11 <cfbriones@uamv.edu.ni>
2024-09-20 08:21:03 -0600

    Ejercicio1

---
+++ OperacionDosNumeros/OperacionDosNumeros/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace OperacionDosNumeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNumero1 = new System.Windows.Forms.TextBox();
+            this.tbNumero2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOperacion = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite un numero:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite otro numero:";
+            // 
+            // tbNumero1
+            // 
+            this.tbNumero1.Location = new System.Drawing.Point(251, 74);
+            this.tbNumero1.Name = "tbNumero1";
+            this.tbNumero1.Size = new System.Drawing.Size(100, 22);
+            this.tbNumero1.TabIndex = 2;
+            // 
+            // tbNumero2
+            // 
+            this.tbNumero2.Location = new System.Drawing.Point(260, 112);
+            this.tbNumero2.Name = "tbNumero2";
+            this.tbNumero2.Size = new System.Drawing.Size(100, 22);
+            this.tbNumero2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Operacion que desea realizar ( + , - , * , / ):";
+            // 
+            // tbOperacion
+            // 
+            this.tbOperacion.Location = new System.Drawing.Point(392, 154);
+            this.tbOperacion.Name = "tbOperacion";
+            this.tbOperacion.Size = new System.Drawing.Size(100, 22);
+            this.tbOperacion.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(137, 195);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(107, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbOperacion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbNumero2);
+            this.Controls.Add(this.tbNumero1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNumero1;
+        private System.Windows.Forms.TextBox tbNumero2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbOperacion;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ OperacionDosNumeros/OperacionDosNumeros/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperacionDosNumeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //  Definir variables
+            double num1, num2, result = 0;
+            string operacion;
+
+            //  Asignar valores a las variables
+            num1 = Convert.ToDouble(tbNumero1.Text);
+            num2 = Convert.ToDouble(tbNumero2.Text);
+            operacion = tbOperacion.Text;
+
+            //  Switch para realizar la operacion deseada de los dos numeros
+            switch (operacion)
+            {
+                case "+":   //  Case para realizar la suma
+                    result = num1 + num2; break;
+                case "-":   //  Case para realizar la resta
+                    result = num1 - num2; break;
+                case "*":   //  Case para realizar la multiplicacion
+                    result = num1 * num2; break;
+                case "/":   //  Case para realizar la division
+                    if (num2 != 0)  //  Si el numero es distinto de 0, se realiza la division
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("ERROR: No se puede dividir entre 0.");
+                    }; break;
+                default:
+                    MessageBox.Show("No se digito ninguna operacion"); return ;
+            }
+
+            //  Se muestra en pantalla el resultado de la operacion
+            label4.Text = "Resultado: " + result.ToString();
+
+        }
+    }
+}
