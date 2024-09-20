0509a911db0fad7f05d2355dcf9836ea6096e810
Zombender <nocqueponer0206@gmail.com>
2024-09-20 08:35:03 -0600

    Ejercicio 4

---
+++ DaniloPrueba/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DaniloPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContrasenya = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(95, 92);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(64, 20);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContrasenya
+            // 
+            this.lblContrasenya.AutoSize = true;
+            this.lblContrasenya.Location = new System.Drawing.Point(80, 163);
+            this.lblContrasenya.Name = "lblContrasenya";
+            this.lblContrasenya.Size = new System.Drawing.Size(92, 20);
+            this.lblContrasenya.TabIndex = 1;
+            this.lblContrasenya.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(197, 89);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(200, 26);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(197, 157);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(200, 26);
+            this.txtContrasena.TabIndex = 3;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(556, 270);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 20);
+            this.lblMensaje.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(197, 223);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(159, 67);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Iniciar Sesion";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContrasenya);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContrasenya;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ DaniloPrueba/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DaniloPrueba
+{
+    public partial class Form1 : Form
+    {
+        int iIntentos = 0;
+        const int iMaxIntentos = 3;
+        const int iUsuarioCorrecto = 12;
+        const int iContrasenaCorrecta = 1234;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Verificar si los valores ingresados son números enteros
+            if (int.TryParse(txtUsuario.Text, out int usuario) &&
+                int.TryParse(txtContrasena.Text, out int contrasena))
+            {
+                // Verificar credenciales
+                if (usuario == iUsuarioCorrecto && contrasena == iContrasenaCorrecta)
+                {
+                    lblMensaje.Text = "Inicio de sesión exitoso.";
+                    lblMensaje.ForeColor = System.Drawing.Color.Green;
+                    btnLogin.Enabled = false; // Deshabilitar el botón después del éxito
+                }
+                else
+                {
+                    iIntentos++;
+                    lblMensaje.Text = $"Credenciales incorrectas. Intento {iIntentos} de {iMaxIntentos}.";
+                    lblMensaje.ForeColor = System.Drawing.Color.Red;
+
+                    // Verificar si se alcanzó el máximo de intentos
+                    if (iIntentos >= iMaxIntentos)
+                    {
+                        MessageBox.Show("Has alcanzado el número máximo de intentos.", "Acceso denegado");
+                        btnLogin.Enabled = false; // Deshabilitar el botón después de alcanzar el máximo de intentos
+                    }
+                }
+            }
+            else
+            {
+                lblMensaje.Text = "Por favor, ingrese valores numéricos.";
+                lblMensaje.ForeColor = System.Drawing.Color.Red;
+
+            }
+        }
+    }
+}
